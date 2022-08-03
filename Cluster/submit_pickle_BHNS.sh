a970db40babe1e065a226b5719b0d25d4acbf035
Adam-Boesky <apboesky@gmail.com>
2022-08-03 19:35:53 -0400

    debug cluster commands

--- Cluster/submit_pickle_BHNS.sh
+++ Cluster/submit_pickle_BHNS.sh
@@ -2,7 +2,7 @@
 #SBATCH -c 96               # Number of cores (-c)
 #SBATCH -t 0-15:00          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -p shared             # Partition to submit to
-#SBATCH --mem=368G          # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --mem=184G          # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o myoutput_%j.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
 module load Anaconda/5.0.1-fasrc02
